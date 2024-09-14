515febdc85801dc77d7188acd9943f7590440db9
Farman1304 <110340146+Farman1304@users.noreply.github.com>
2024-09-14 19:47:52 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+var city = prompt ("Enter city")
+
+if(city === "Karachi") {
+    alert ("welcom to the city of lights")
+}
+
+
+var gender = prompt ("Enter your gender");
+
+if ( gender === "Male") {
+    alert('Good Morning Sir')
+} else if (gender === "Female") {
+    alert ("Good Morning Ma'am")
+}
+
+var color = prompt ("Enter your desire color")
+
+if ( color === "Red") {
+    alert ("Must stop")
+} else if ( color === "Yellow")
+alert(" Ready to move")
+else if ( color ==="Green") {
+    alert("Move now") }
+    else {
+        alert ("Be car ful")
+    }
